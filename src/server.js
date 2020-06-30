10d4d12816713883787462ff047f47c3efbff495
Odalys Paz Mendoza <odhalyxzpaz@gmail.com>
2020-06-30 15:22:40 -0500

    Manejo de CORS

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.set('port', process.env.PORT || 4000);
 
 // CORS
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');   
 
     // authorized headers for preflight requests
@@ -23,8 +23,16 @@ app.use((req, res, next) => {
         res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE');
         res.send();
     });
-});
+});*/
 
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+});
 // Middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
